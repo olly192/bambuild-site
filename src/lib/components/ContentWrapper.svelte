5bab56063ee57dca95199987c1eb0243a3419bb3
Olly <oliverpm7@gmail.com>
2022-12-05 20:25:05 +0000

    Update breadcrumbs

--- src/lib/components/ContentWrapper.svelte
+++ src/lib/components/ContentWrapper.svelte
@@ -2,21 +2,60 @@
     import { page } from '$app/stores';
     import Footer from "$lib/components/Footer.svelte";
     export let breadcrumb = false
-    let breadcrumbs = $page.route.id.split('/').slice(1)
     let crumbData = {
-        'shop': {title: 'Shop', href: '/shop'},
-        'product': {title: 'Product', href: ''},
-        'lightbox': {title: 'Lightbox', href: ''},
-        'christmas-bundle': {title: 'Lightbox Christmas Bundle', href: '/shop/product/lightbox/christmas-bundle'},
+        'order': {
+            'title': 'Order',
+            'href': '',
+            'lightbox': {
+                'title': 'Lightbox',
+                'href': '',
+                'christmas-bundle': {
+                    'title': 'Christmas Bundle',
+                    'href': '/order/lightbox/christmas-bundle'
+                }
+            }
+        },
+        'shop': {
+            'title': 'Shop',
+            'href': '/shop',
+            'product': {
+                'title': 'Product',
+                'href': '',
+                'lightbox': {
+                    'title': 'Lightbox',
+                    'href': '/shop/product/lightbox',
+                    'christmas-bundle': {
+                        'title': 'Christmas Bundle',
+                        'href': '/shop/product/lightbox/christmas-bundle'
+                    }
+                }
+            }
+        }
     }
-    let crumbs = breadcrumbs.map(item => crumbData[item] || item)
+    let crumbResolver = () => {
+        let crumbs = []
+        let breadcrumbs = $page.route.id.split('/').slice(1)
+        let path = []
+        breadcrumbs.forEach((item) => {
+            path.push(item)
+            let itemData = crumbData
+            path.forEach((pathItem) => {
+                itemData = itemData[pathItem]
+            })
+            crumbs.push({
+                'title': itemData.title,
+                'href': itemData.href
+            })
+        })
+        return crumbs
+    }
 </script>
 
 <div class="p-8 mt-16">
     {#if breadcrumb}
         <div class="text-sm breadcrumbs">
             <ul>
-                {#each crumbs as {title, href}}
+                {#each crumbResolver() as {title, href}}
                     {#if href}
                         <li><a href="{href}">{title}</a></li>
                     {:else}
