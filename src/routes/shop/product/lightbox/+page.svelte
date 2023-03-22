d966527b34327c2c8444cad09cd4b4c064a185a4
Olly <oliverpm7@gmail.com>
2023-03-22 11:56:30 +0000

    Remove preorder notice

--- src/routes/shop/product/lightbox/+page.svelte
+++ src/routes/shop/product/lightbox/+page.svelte
@@ -31,10 +31,6 @@
         <div id="options-container">
             <div id="options-card">
                 <div>
-                    <Alert color="warning">
-                        <p class="font-bold">Currently on pre-order</p>
-                        <p>Expected delivery date: 12/12/2022</p>
-                    </Alert>
                     <h3 class="heading-3">Description</h3>
                     <p>
                         Build your own light box with the BamBuild Light Box Bundle.
